def327d6b88911b4287452a3312def893dce0b1a
jspc <james@zero-internet.org.uk>
2018-06-30 16:30:35 +0100

    Listen and Serve

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/miekg/dns"
+)
+
+var (
+	domain   = flag.String("domain", "internal.", "Domain name with which to serve records on")
+	target   = flag.String("target", "localhost", "Address (host, ipv4, ipv6 etc.) to respond to requests with")
+	resolver = flag.String("resolver", "1.1.1.1:53", "DNS Server to pass reqeusts to which are not on our domain")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Print("Starting")
+
+	docker, err := NewDocker()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	d, err := NewDNS(*domain, *target, *resolver, docker)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	panic(dns.ListenAndServe(":3553", "udp", d))
+}
